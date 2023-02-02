b9480923a8464319c162af4d7a5ba24d1eb10149
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-02 20:52:00 +0000

    Display current and 5-day forecast

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,6 @@
 // Add your own API key between the ""
 let APIKey = "a4cd937b9cbcabbb845e8b24815e4ab0";
-let coordinates = [0,0];
+let coordinates = [0, 0];
 
 // We then created an AJAX call
 /*$.ajax({
@@ -20,11 +20,11 @@ let coordinates = [0,0];
 
 });*/
 
-function getCoordinates(queryCoordURL){
+function getCoordinates(queryCoordURL) {
     $.ajax({
         url: queryCoordURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response[0].lat);
         console.log(response[0].lon);
         let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=" + APIKey;
@@ -36,18 +36,36 @@ function getForecast(queryForecastURL) {
     $.ajax({
         url: queryForecastURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
+        displayCurrentWeather(response);
         displayForecast(response);
     })
 }
 
+function displayCurrentWeather(response) {
+    console.log(response);
+    console.log(response.city.name);
+    $("#city").text(response.city.name);
+    let time = moment(response.list[0].dt, "X");
+    $("#date").text(" (" + time.format("DD-MM-YYYY") + ")");
+    $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+    $("#current-temp").text((response.list[0].main.temp - 273.15).toFixed(2));
+    $("#current-wind").text((response.list[0].wind.speed * 3.6).toFixed(2));
+    $("#current-humidity").text(response.list[0].main.humidity);
+}
+
 function displayForecast(response) {
-    for(let i=0;i<6;i++){
-        console.log(response.list[i].main);
+    for (let i = 1; i < 6; i++) {
+        let time = moment(response.list[(i-1)*8+1].dt, "X");
+        $("#date"+i).text(time.format("DD-MM-YYYY"));
+        $("#weather-icon"+i).attr("src", "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
+        $("#current-temp"+i).text((response.list[i].main.temp - 273.15).toFixed(2));
+        $("#current-wind"+i).text((response.list[i].wind.speed * 3.6).toFixed(2));
+        $("#current-humidity"+i).text(response.list[i].main.humidity);
     }
 }
 
-$("#search-button").on("click", function(event){
+$("#search-button").on("click", function (event) {
     event.preventDefault();
     // Here we are building the URL we need to query the database
     console.log($("#search-input").val().trim());
