7f159f6a4aaf8facf62792dc90b6cfa8284e2b34
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-02 19:22:45 +0000

    Get and log weather information on console

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,9 @@
 // Add your own API key between the ""
-var APIKey = "a4cd937b9cbcabbb845e8b24815e4ab0";
+let APIKey = "a4cd937b9cbcabbb845e8b24815e4ab0";
+let coordinates = [0,0];
 
-// Here we are building the URL we need to query the database
-var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val().trim() + "&limit=1&appid=" + APIKey;
-console.log(queryURL);
 // We then created an AJAX call
-$.ajax({
+/*$.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
@@ -20,4 +18,39 @@ $.ajax({
   // Hint: To convert from Kelvin to Celsius: C = K - 273.15
   // Create CODE HERE to dump the temperature content into HTML
 
-});
+});*/
+
+function getCoordinates(queryCoordURL){
+    $.ajax({
+        url: queryCoordURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response[0].lat);
+        console.log(response[0].lon);
+        let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=" + APIKey;
+        getForecast(queryForecastURL);
+    })
+}
+
+function getForecast(queryForecastURL) {
+    $.ajax({
+        url: queryForecastURL,
+        method: "GET"
+    }).then(function(response){
+        displayForecast(response);
+    })
+}
+
+function displayForecast(response) {
+    for(let i=0;i<6;i++){
+        console.log(response.list[i].main);
+    }
+}
+
+$("#search-button").on("click", function(event){
+    event.preventDefault();
+    // Here we are building the URL we need to query the database
+    console.log($("#search-input").val().trim());
+    var queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val().trim() + "&limit=1&appid=" + APIKey;
+    getCoordinates(queryCoordURL);
+})
