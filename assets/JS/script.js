b9a8b4ae41d56977b1f7886727fbc108d10f0630
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-03 19:48:41 +0000

    Search History

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,31 +2,23 @@
 let APIKey = "a4cd937b9cbcabbb845e8b24815e4ab0";
 let coordinates = [0, 0];
 
-// We then created an AJAX call
-/*$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(queryURL);
-  console.log(response);
-  $(".city").text("City: " + response.name);
-  $(".temp").text("Temperature: " + response.main.temp);
-  // Create CODE HERE to Log the queryURL
-  // Create CODE HERE to log the resulting object
-  // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  // Create CODE HERE to transfer content to HTML
-  // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-  // Create CODE HERE to dump the temperature content into HTML
+let searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+if (searchHistory == null) {
+    searchHistory = [];
+}else{
+    let cityName = searchHistory[0];
+    let queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
+    getCoordinatesAndForecast(queryCoordURL);
+    addToSearchHistory();
+}
 
-});*/
-
-function getCoordinates(queryCoordURL) {
+function getCoordinatesAndForecast(queryCoordURL) {
     $.ajax({
         url: queryCoordURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response[0].lat);
-        console.log(response[0].lon);
+        //console.log(response[0].lat);
+        //console.log(response[0].lon);
         let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=" + APIKey;
         getForecast(queryForecastURL);
     })
@@ -43,13 +35,13 @@ function getForecast(queryForecastURL) {
 }
 
 function displayCurrentWeather(response) {
-    console.log(response);
+    //console.log(response);
     console.log(response.city.name);
     $("#city").text(response.city.name);
     let time = moment(response.list[0].dt, "X");
     $("#date").text(" (" + time.format("DD-MM-YYYY") + ")");
     $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
-    $("#current-temp").text((response.list[0].main.temp - 273.15).toFixed(2));
+    $("#current-temp").text((response.list[0].main.temp - 273.15).toFixed(0));
     $("#current-wind").text((response.list[0].wind.speed * 3.6).toFixed(2));
     $("#current-humidity").text(response.list[0].main.humidity);
 }
@@ -59,16 +51,56 @@ function displayForecast(response) {
         let time = moment(response.list[(i-1)*8+1].dt, "X");
         $("#date"+i).text(time.format("DD-MM-YYYY"));
         $("#weather-icon"+i).attr("src", "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
-        $("#current-temp"+i).text((response.list[i].main.temp - 273.15).toFixed(2));
+        $("#current-temp"+i).text((response.list[i].main.temp - 273.15).toFixed(0));
         $("#current-wind"+i).text((response.list[i].wind.speed * 3.6).toFixed(2));
         $("#current-humidity"+i).text(response.list[i].main.humidity);
     }
 }
 
+function addToSearchHistory() {
+    $("#history").empty();
+    for(let i=0; i<searchHistory.length; i++) {
+        let button = $("<button>");
+        button.attr("class","btn btn-secondary w-100 mt-1 mb-1");
+        button.text(searchHistory[i]);
+        $("#history").append(button);
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function searchHistoryContains(cityName) {
+    for(i=0;i<searchHistory.length;i++){
+        if(searchHistory[i]===cityName){
+            return true;
+        }
+    }
+    return false;
+}
+
 $("#search-button").on("click", function (event) {
     event.preventDefault();
     // Here we are building the URL we need to query the database
-    console.log($("#search-input").val().trim());
-    var queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val().trim() + "&limit=1&appid=" + APIKey;
-    getCoordinates(queryCoordURL);
+    let cityName = $("#search-input").val().trim();
+    console.log(cityName);
+    let queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val().trim() + "&limit=1&appid=" + APIKey;
+    getCoordinatesAndForecast(queryCoordURL);
+    if(!searchHistoryContains(capitalizeFirstLetter(cityName)))
+        searchHistory.push(capitalizeFirstLetter(cityName));
+    addToSearchHistory();
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+})
+
+$("#history").on("click", "button", function (event) {
+    event.preventDefault();
+    // Here we are building the URL we need to query the database
+    let cityName = event.target.textContent;
+    console.log(cityName);
+    let queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
+    getCoordinatesAndForecast(queryCoordURL);
+    $("#search-input").val(cityName);
+    searchHistory.push(cityName);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 })
