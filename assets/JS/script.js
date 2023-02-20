d1151860f24fffff9aa363b4e9285675eaea7e53
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-20 16:37:47 +0000

    Changed http: to https:

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,7 +8,7 @@ if (searchHistory == null) {
     searchHistory = [];
 }else{
     let cityName = searchHistory[0];
-    let queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
+    let queryCoordURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
     getCoordinatesAndForecast(queryCoordURL);
     addToSearchHistory();
 }
@@ -20,7 +20,7 @@ function getCoordinatesAndForecast(queryCoordURL) {
         url: queryCoordURL,
         method: "GET"
     }).then(function (response) {
-        let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=" + APIKey;
+        let queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=" + APIKey;
         $("#city").text(response[0].name);
         getForecast(queryForecastURL);
     })
@@ -43,7 +43,7 @@ function displayCurrentWeather(response) {
 
     let time = moment(response.list[0].dt, "X");
     $("#date").text(" (" + time.format("DD-MM-YYYY") + ")");
-    $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+    $("#weather-icon").attr("src", "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
     $("#current-temp").text((response.list[0].main.temp - 273.15).toFixed(0));
     $("#current-wind").text((response.list[0].wind.speed * 3.6).toFixed(2));
     $("#current-humidity").text(response.list[0].main.humidity);
@@ -55,7 +55,7 @@ function displayForecast(response) {
     for (let i = 1; i < 6; i++) {
         let time = moment(response.list[(i-1)*8+2].dt, "X");
         $("#date"+i).text(time.format("DD-MM-YYYY"));
-        $("#weather-icon"+i).attr("src", "http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
+        $("#weather-icon"+i).attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
         $("#current-temp"+i).text((response.list[i].main.temp - 273.15).toFixed(0));
         $("#current-wind"+i).text((response.list[i].wind.speed * 3.6).toFixed(2));
         $("#current-humidity"+i).text(response.list[i].main.humidity);
@@ -98,7 +98,7 @@ $("#search-button").on("click", function (event) {
     // Building the URL we need to query the database to get the coordinates of the place
     let cityName = $("#search-input").val().trim();
     console.log(cityName);
-    let queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val().trim() + "&limit=1&appid=" + APIKey;
+    let queryCoordURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val().trim() + "&limit=1&appid=" + APIKey;
 
     // Get Coordinates of the place and then get the forecast
     getCoordinatesAndForecast(queryCoordURL);
@@ -117,7 +117,7 @@ $("#history").on("click", "button", function (event) {
     // Building the URL we need to query the database to get the coordinates of the place
     let cityName = event.target.textContent;
     console.log(cityName);
-    let queryCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
+    let queryCoordURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
     
     // Get Coordinates of the place and then get the forecast
     getCoordinatesAndForecast(queryCoordURL);
